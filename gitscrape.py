0191455a9692f13ae0b2db707530650cbf95d861
marcolussetti <git@marcolussetti.com>
2022-10-07 22:08:09 -0700

    re-enable collection, split into multiple files

--- gitscrape.py
+++ gitscrape.py
@@ -5,52 +5,51 @@ import pandas as pd
 import numpy as np
 import itertools
 
-urls = [
-        "https://www.bcliquorstores.com/ajax/browse?category=wine&sort=name.raw:asc&size=5000",
-        "https://www.bcliquorstores.com/ajax/browse?category=beer&sort=name.raw:asc&size=5000",
-        "https://www.bcliquorstores.com/ajax/browse?category=spirits&sort=name.raw:asc&size=5000",
-        "https://www.bcliquorstores.com/ajax/browse?category=coolers/ciders&sort=name.raw:asc&size=5000"
-        ]
+base_url = "https://www.bcliquorstores.com/ajax/browse?category=???&sort=name.raw:asc&size=5000"
 
-items = list(itertools.chain(*[requests.get(url).json()["hits"]["hits"] for url in urls]))
+categories = ["wine", "beer", "spirits", "coolers"]
 
-df_items = pd.DataFrame(items)
-df_exploded = pd.DataFrame(df_items['_source'].tolist())
-df = df_items.merge(df_exploded, left_on="_id", right_on="sku")
+for category in categories:
 
-# Explode some more columns
-df['category_id'] = df['category'].apply(lambda cell: cell['id'])
-df['category'] = df['category'].apply(lambda cell: cell['description'])
-df['subCategory_id'] = df['subCategory'].apply(lambda cell: cell['id'])
-df['subCategory'] = df['subCategory'].apply(lambda cell: cell['description'])
-df['class_id'] = df['class'].apply(lambda cell: cell['id'])
-df['class'] = df['class'].apply(lambda cell: cell['description'])
-df['sort'] = df['sort'].apply(lambda cell: cell[0])
+    items = requests.get(base_url.replace("???", category)).json()["hits"]["hits"]
 
-# Convert columns
-df['alcoholPercentage'] = df['alcoholPercentage'].apply(lambda cell: cell/100)
-df['regularPrice'] = df['regularPrice'].apply(lambda cell: float(cell))
-# Convert to float
-for col in ['volume', 'regularPrice', '_currentPrice', '_regularPrice', '_score', 'currentPrice']:
-    df[col] = df[col].apply(lambda cell: float(cell))
+    df_items = pd.DataFrame(items)
+    df_exploded = pd.DataFrame(df_items['_source'].tolist())
+    df = df_items.merge(df_exploded, left_on="_id", right_on="sku")
 
-# Composite columns
+    # Explode some more columns
+    df['category_id'] = df['category'].apply(lambda cell: cell['id'])
+    df['category'] = df['category'].apply(lambda cell: cell['description'])
+    df['subCategory_id'] = df['subCategory'].apply(lambda cell: cell['id'])
+    df['subCategory'] = df['subCategory'].apply(lambda cell: cell['description'])
+    df['class_id'] = df['class'].apply(lambda cell: cell['id'])
+    df['class'] = df['class'].apply(lambda cell: cell['description'])
+    df['sort'] = df['sort'].apply(lambda cell: cell[0])
 
-# Total volume
-df['totalVolume'] = df['unitSize'] * df['volume'] * df['availableUnits']
-# Total value
-df['totalPrice'] = df['availableUnits'] * df['regularPrice']
-# Total alcohol content in L
-df['totalAlcoholContent'] = df['unitSize'] * df['volume'] * df['alcoholPercentage']
-# Price per alcohol content in L
-df['alcoholCost'] = df['regularPrice'] / df['totalAlcoholContent']
-# Hardly legit, but cost by volume weighted by rating
-#df['costPerRating'] = df['regularPrice'] / (df['unitSize'] * df['volume']) / df['consumerRating']
-# Store percentage
-df['storePercent'] = df['storeCount'] / df['storeCount'].max()
+    # Convert columns
+    df['alcoholPercentage'] = df['alcoholPercentage'].apply(lambda cell: cell/100)
+    df['regularPrice'] = df['regularPrice'].apply(lambda cell: float(cell))
+    # Convert to float
+    for col in ['volume', 'regularPrice', '_currentPrice', '_regularPrice', '_score', 'currentPrice']:
+        df[col] = df[col].apply(lambda cell: float(cell))
 
-# Drop columns
-df = df.drop(columns=['_currentPrice', '_regularPrice', '_source'])
+    # Composite columns
 
-df.to_csv("bcliquordata.csv")
-df.to_json("bcliquordata.json", orient='records', lines=True)
+    # Total volume
+    df['totalVolume'] = df['unitSize'] * df['volume'] * df['availableUnits']
+    # Total value
+    df['totalPrice'] = df['availableUnits'] * df['regularPrice']
+    # Total alcohol content in L
+    df['totalAlcoholContent'] = df['unitSize'] * df['volume'] * df['alcoholPercentage']
+    # Price per alcohol content in L
+    df['alcoholCost'] = df['regularPrice'] / df['totalAlcoholContent']
+    # Hardly legit, but cost by volume weighted by rating
+    #df['costPerRating'] = df['regularPrice'] / (df['unitSize'] * df['volume']) / df['consumerRating']
+    # Store percentage
+    df['storePercent'] = df['storeCount'] / df['storeCount'].max()
+
+    # Drop columns
+    df = df.drop(columns=['_currentPrice', '_regularPrice', '_source'])
+
+    df.to_csv(f"bcliquordata-{category}.csv")
+    df.to_json(f"bcliquordata-{category}.json", orient='records', lines=True)
